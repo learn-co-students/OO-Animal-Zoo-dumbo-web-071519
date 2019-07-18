f8f1e2578447f5fa13d8cdf99d6e75b3c9397ca1
Ross Perkel <rossperkel@gmail.com>
2019-07-18 14:50:55 -0400

    alessandra vertrees and my lab

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,28 @@
 class Animal
+    @@all = []
+    attr_accessor :weight
+    attr_reader :species, :nickname, :zoo
 
-end
+    def initialize(species, weight, nickname, zoo)
+        @species = species
+        @weight = weight
+        @nickname = nickname
+        @@all << self
+        @zoo = zoo
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_species(species)
+        @@all.select do |animal|
+            animal.species == species
+        end
+    end
+
+    def self.species
+        @species
+    end
+
+end 
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,43 @@
 class Zoo
+    @@all = []
+    attr_reader :name, :location
 
+    def initialize(name, location)
+            @name = name
+            @location = location 
+            @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_location(location)
+        @@all.select do |zoo|
+            zoo.location == location
+        end
+    end
+
+    def animals
+        Animal.all.select do |animal|
+            animal.zoo == self
+        end  
+    end
+
+    def animal_species
+        animalArr = self.animals.map do |animal| animal.species end
+        animalArr.uniq
+    end
+
+    def animal_nicknames
+        self.animals.map do |animal| animal.nickname end
+    end
+
+    def find_by_species(species)
+        self.animals.select do |animal|
+            animal.species == species
+        end
+    end
+
+    
 end
