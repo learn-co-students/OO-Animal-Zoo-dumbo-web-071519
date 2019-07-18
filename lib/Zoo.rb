ca5f3d6071a4249a70b0c2065540c3b7feea5aa2
Ashna Shah-Grover <ashnashahgrover777@gmail.com>
2019-07-18 12:30:09 -0400

    Most Recent Working Version

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,25 @@
 class Zoo
 
+  attr_reader :location, :name
+
+  @@all = []
+
+  def initialize(name, location)
+    @name = name
+    @location = location
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def animals
+    Animal.all.select {|animal| animal.zoo==self}
+  end
+
+  def species
+    
+  end
+
 end
--- run.rb
+++ run.rb
@@ -5,6 +5,12 @@ require 'pry'
 
 #Test your code here
 
+Zootopia = Zoo.new("Zootopia", "Pluto")
+TheBronxZoo = Zoo.new("Bronx Zoo", "Bronx")
+
+Bunny = Animal.new("Bunny", 5, "Snowflake", Zootopia)
+Giraffe = Animal.new("Giraffe", 1800, "Jared", TheBronxZoo)
+Flamingo = Animal.new("Flamingo", 10, "Pinky", Zootopia)
 
 binding.pry
 puts "done"
