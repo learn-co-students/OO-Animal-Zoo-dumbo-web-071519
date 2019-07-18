bc1d55194fbf788dfe23a103935678375a8075ec
Flatiron School <oscar_ochoac@yahoo.com>
2019-07-18 16:05:09 -0400

    Changes

--- lib/Animal.rb
+++ lib/Animal.rb
@@ -1,3 +1,40 @@
 class Animal
 
+    @@all = []
+
+    attr_reader :nickname, :species, :weight, :zoo
+    attr_writer :weight, :zoo
+
+    def initialize(nickname, species, weight, zoo)
+        @nickname = nickname
+        @species = species
+        @weight = weight
+        @zoo = zoo #Zoo Object
+
+        @@all << self
+    end
+
+    def nickname
+        @nickname
+    end
+
+    def weight
+        @weight
+    end
+
+    def species
+        @species
+    end
+
+    def self.all
+        @@all
+    end
+
+    def find_by_species
+
+    end
+
 end
+
+
+
--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,34 @@
 class Zoo
 
+@@all = []
+
+    def initialize(name, location)
+        @name = name
+        @location = location
+
+        @@all << self
+    end
+
+    def location()
+        @location
+    end
+
+    def name()
+        @name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def animals
+        Animal.all.select do |animal|
+            animal.zoo == self
+        end
+    end 
+ 
+
+
 end
+
+
