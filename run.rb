c62423c5d338de6d8690df6fb40f975cb3595ccc
CihatAld <cihataldanmaz@gmail.com>
2019-07-21 19:22:28 -0400

    Done.

--- run.rb
+++ run.rb
@@ -2,7 +2,19 @@ require_relative "lib/Animal.rb"
 require_relative "lib/Zoo.rb"
 require 'pry'
 
+zoo1 = Zoo.new("Brkyln", "Dumbo")
+zoo2 = Zoo.new("Kweens", "Sunnyside")
 
+
+bob = Animal.new("cat", "bob", 8, zoo1)
+loos = Animal.new("dog", "loss", 15, zoo1)
+kitty = Animal.new("cat", "kitty", 19, zoo2)
+lilly = Animal.new("cat", "milly", 121, zoo1)
+puppy = Animal.new("dog", "puppy", 29, zoo1)
+kitty = Animal.new("cat", "kitty", 19, zoo2)
+duckki = Animal.new("dog", "duckki", 29, zoo2)
+
+binding.pry
 #Test your code here
 
 
